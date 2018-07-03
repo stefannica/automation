77645ece8bd5c0f070fde725153d04540a76b9e0
Stefan Nica <snica@suse.com>
2018-07-03 12:11:13 +0200

    Some stuff around templating the service models used to generate input models

--- scripts/jenkins/ardana/templates/library/load_input_model.py
+++ scripts/jenkins/ardana/templates/library/load_input_model.py
@@ -20,134 +20,22 @@ DOCUMENTATION = '''
 module: load_input_model
 short_description: Load an Ardana input model
 description: |
-  Load an Ardana input model from a directory structure
+  Load an Ardana input model from a given path
 author: SUSE Linux GmbH
 options:
   path:
-    description: Root path where the input model is located 
+    description: Root path where the input model files are located 
 '''
 
 EXAMPLES = '''
 - load_input_model:
     path: path/to/input/model
-  register: input_model
-- debug: msg="{{ input_model.input_model }}"
+  register: _result
+- debug: msg="{{ _result.input_model }}"
 '''
 
 import os, yaml
 from collections import OrderedDict
-
-
-def enhance_input_model(input_model, keep_refs=False):
-    """
-    Enhance the input model structure:
-      - use dictionaries instead of lists
-      - use references instead of attributes
-      - delete unused elements
-
-    :param input_model: the input model loaded from disk
-    :param keep_refs: set to True to keep circular references in
-    the input model (which is fatal if the input model is returned to ansible).
-    """
-
-    def convert_list_attr_to_map(element, attr_name, key='name'):
-        if attr_name in element:
-            element[attr_name] = {item[key]: item for item in element[attr_name]}
-        else:
-            element[attr_name] = {}
-
-    def link_attr_to_element(element, attr_name, target_map, ref_list='refs'):
-        key = element.setdefault(attr_name)
-        if key and not isinstance(key, dict) and key in target_map:
-            element[attr_name] = target_map[key]
-            target_map[key].setdefault(ref_list, []).append(element)
-
-    def link_attr_list_to_element(element, attr_name, target_map, ref_list='refs'):
-        key_list = element.setdefault(attr_name, [])
-        for key in key_list:
-            if key and isinstance(key, dict) and key in target_map:
-                element[attr_name] = target_map[key]
-                target_map[key].setdefault(ref_list, []).append(element)
-
-    def prune_unused_items(element_map):
-        for name, element in element_map.items():
-            if not element.get('refs'):
-                del element_map[name]
-            elif not keep_refs:
-                # Delete gathered references to avoid circular references
-                del element['refs']
-
-    for top_level_list in ['control-planes',
-                           'configuration-data',
-                           'server-roles',
-                           'disk-models',
-                           'interface-models',
-                           'networks',
-                           'network-groups',
-                           'nic-mappings',
-                           'servers',
-                           'server-groups',
-                           'firewall-rules']:
-        convert_list_attr_to_map(input_model, top_level_list,
-                                 'id' if top_level_list == 'servers' else 'name')
-
-    load_balancers = {}
-    for cp in input_model['control-planes'].itervalues():
-        convert_list_attr_to_map(cp, 'load-balancers')
-        convert_list_attr_to_map(cp, 'clusters')
-        convert_list_attr_to_map(cp, 'resources')
-        link_attr_list_to_element(cp, 'configuration-data', input_model['configuration-data'])
-        load_balancers.update(cp['load-balancers'])
-        for cluster in cp['clusters'].itervalues():
-            link_attr_to_element(cluster, 'server-role', input_model['server-roles'])
-            link_attr_list_to_element(cluster, 'configuration-data', input_model['configuration-data'])
-        for resource in cp['resources'].itervalues():
-            link_attr_to_element(resource, 'server-role', input_model['server-roles'])
-            link_attr_list_to_element(cluster, 'configuration-data', input_model['configuration-data'])
-
-    prune_unused_items(input_model['configuration-data'])
-    prune_unused_items(input_model['server-roles'])
-
-    for server_role in input_model['server-roles'].itervalues():
-        link_attr_to_element(server_role, 'interface-model', input_model['interface-models'])
-        link_attr_to_element(server_role, 'disk-model', input_model['disk-models'])
-
-    prune_unused_items(input_model['interface-models'])
-    prune_unused_items(input_model['disk-models'])
-
-    for server_id, server in input_model['servers'].items():
-        link_attr_to_element(server, 'role', input_model['server-roles'])
-        if not server['role']:
-            # Keep only those servers that are actually required
-            del input_model['servers'][server_id]
-            continue
-        link_attr_to_element(server, 'nic-mapping', input_model['nic-mappings'])
-
-    prune_unused_items(input_model['nic-mappings'])
-
-    for interface_model in input_model['interface-models'].itervalues():
-        convert_list_attr_to_map(interface_model, 'network-interfaces')
-        for interface in interface_model['network-interfaces'].itervalues():
-            link_attr_list_to_element(interface, 'network-groups', input_model['network-groups'])
-            link_attr_list_to_element(interface, 'forced-network-groups', input_model['network-groups'])
-
-    prune_unused_items(input_model['network-groups'])
-
-    for firewall_rule_id, firewall_rule in input_model['firewall-rules'].items():
-        link_attr_list_to_element(firewall_rule, 'network-groups', input_model['network-groups'])
-        if None in firewall_rule['network-groups']:
-            # Keep only those firewall rules that are actually in use
-            del input_model['firewall-rules'][firewall_rule_id]
-            continue
-
-    for network_group in input_model['network-groups'].itervalues():
-        link_attr_list_to_element(network_group, 'load-balancers', load_balancers)
-
-    # remove remaining circular references
-    prune_unused_items(load_balancers)
-    for top_level_list in ['network-groups',
-                           'server-roles']:
-        prune_unused_items(input_model[top_level_list])
 
 
 def merge_input_model(data, input_model):
@@ -188,7 +76,6 @@ def main():
                         input_model = load_input_model_file(file_name, input_model)
             else:
                 input_model = load_input_model_file(input_model_path, input_model)
-            enhance_input_model(input_model)
     except Exception as e:
         module.fail_json(msg=e.message)
     module.exit_json(rc=0, changed=False, input_model=input_model)
